37c61b2c915ad1210d65ce8a5acbc65ad283a0a3
guru0610 <52495910+guru0610@users.noreply.github.com>
2019-07-12 12:21:06 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+subscription_id = "0d3a3a2b-2635-4743-84b7-aaa5e69fe5ba"
+client_id = "${var.azure_client_id}"
+client_secret = "${var.azure_client_secret}"
+tenant_id = "0ae51e19-07c8-4e4b-bb6d-648ee58410f4"
+}
+resource "azurerm_resource_group" "cicd" {
+  name     = "cicd02"
+  location = "southeastasia"
+
+  tags = {
+    environment = "Production"
+  }
+}
